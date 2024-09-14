7d1e64c9ca6d8b1d173267223ce4d8bab415a853
kvo5 <kvo5@uncc.edu>
2024-09-13 22:15:12 -0400

    Views & URL's

--- studybud2/urls.py
+++ studybud2/urls.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# from django.http import HttpResponse
+
+# def home(request):
+#     return HttpResponse('Home page')
+
+# def room(request):
+#     return HttpResponse('ROOM')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('base.urls'))
+    # path('', home),
+    # path('room/', room)
 ]
